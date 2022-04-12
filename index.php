c9d2873fae8d5aa5323a4bb594c3bdd68f078987
Kanhaiya97 <89339317+Kanhaiya97@users.noreply.github.com>
2022-04-12 16:11:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require("header.php");
+require("banner.php");
+require("about.php");
+require("middle_section.php");
+require("courses.php");
+require("trainer.php");
+require("footer.php");
+?>
