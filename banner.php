b84d383682f13e7f989f01fcad827912528cd0e8
Kanhaiya97 <89339317+Kanhaiya97@users.noreply.github.com>
2022-04-12 18:12:00 +0530

    Delete banner.php

--- banner.php
+++
@@ -1,8 +0,0 @@
-<!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex justify-content-center align-items-center">
-    <div class="container position-relative" data-aos="zoom-in" data-aos-delay="100">
-      <h1>Hello Learning Today,<br>Leading Tomorrow</h1>
-      <h2>Hello I Am Kanhaiya I am Trying to Make my First Websites </h2>
-      <a href="courses.php" class="btn-get-started">Get Started</a>
-    </div>
-  </section><!-- End Hero -->
