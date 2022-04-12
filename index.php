c1b15257ca7cfa1b0da99cd752795aca7e4b3d60
Kanhaiya97 <89339317+Kanhaiya97@users.noreply.github.com>
2022-04-12 18:15:21 +0530

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("header.php");
-require("banner.php");
-require("about.php");
-require("middle_section.php");
-require("courses.php");
-require("trainer.php");
-require("footer.php");
-?>
